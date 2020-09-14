9ce33f6f557b303d15fbe2abbd4e308c931ae5d0
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-14 17:12:19 -0300

    teste deveCalcularSalarioParaDesenvolvedoresComSalarioAcimaDoLimite

--- src/test/java/teste/CalculadoraDeSalarioTest.java
+++ src/test/java/teste/CalculadoraDeSalarioTest.java
@@ -22,6 +22,17 @@ public class CalculadoraDeSalarioTest {
         double salario = calculadora.calculaSalario(desenvolvedor);
         assertEquals(1500.0 * 0.9, salario, 0.00001);
     }
+    
+    @Test
+    public void deveCalcularSalarioParaDesenvolvedoresComSalarioAcimaDoLimite() {
+        //cenário do desconto de 20% se salario for maior que 3.000,00
+        CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
+
+        Funcionario desenvolvedor = new Funcionario("Mauricio", 4000.0, Cargo.DESENVOLVEDOR);
+
+        double salario = calculadora.calculaSalario(desenvolvedor);
+        assertEquals(4000.0 * 0.8, salario, 0.00001);
+    }
 
     
 }
