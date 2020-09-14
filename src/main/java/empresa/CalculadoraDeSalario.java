16fa44224fa6828634e028c68ef10ec9e18afc16
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-13 22:41:27 -0300

    refarotoração calculaSalario

--- src/main/java/empresa/CalculadoraDeSalario.java
+++ src/main/java/empresa/CalculadoraDeSalario.java
@@ -8,6 +8,9 @@ package empresa;
 public class CalculadoraDeSalario {
 
     public double calculaSalario(Funcionario desenvolvedor) {
+        if(desenvolvedor.getSalario() > 3000.00){
+            return 3200.00;
+        }
         return 1350.0;
     }
     
