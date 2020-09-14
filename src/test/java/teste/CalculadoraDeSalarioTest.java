36032154e25b8d8840c7cbf98505bb8291eb1536
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-14 17:06:36 -0300

    usando baby steps conciente

--- src/test/java/teste/CalculadoraDeSalarioTest.java
+++ src/test/java/teste/CalculadoraDeSalarioTest.java
@@ -23,25 +23,5 @@ public class CalculadoraDeSalarioTest {
         assertEquals(1500.0 * 0.9, salario, 0.00001);
     }
 
-    @Test
-    public void deveCalcularSalarioParaDesenvolvedoresComSalarioAcimaDoLimite() {
-        //cenário do desconto de 20% se salario for maior que 3.000,00
-        CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
-
-        Funcionario desenvolvedor = new Funcionario("Mauricio", 4000.0, Cargo.DESENVOLVEDOR);
-
-        double salario = calculadora.calculaSalario(desenvolvedor);
-        assertEquals(4000.0 * 0.8, salario, 0.00001);
-    }
-
-    @Test
-    public void deveCalcularSalarioParaDBAsComSalarioAbaixoDoLimite() {
-        CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
-        
-        Funcionario dba = new Funcionario("Mauricio", 500.0, Cargo.DBA);
-        
-        double salario = calculadora.calculaSalario(dba);
-        assertEquals(500.0 * 0.85, salario, 0.00001);
-    }
-
+    
 }
