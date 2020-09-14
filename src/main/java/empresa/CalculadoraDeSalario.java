9f5660a7b73f90d2ab1cb007e0280f75e7da00e2
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-14 17:14:56 -0300

    implementação correta

--- src/main/java/empresa/CalculadoraDeSalario.java
+++ src/main/java/empresa/CalculadoraDeSalario.java
@@ -8,6 +8,10 @@ package empresa;
 public class CalculadoraDeSalario {
 
     public double calculaSalario(Funcionario funcionario) {
+        
+        if(funcionario.getSalario() > 3000){
+            return funcionario.getSalario() * 0.8;
+        }
         return funcionario.getSalario() * 0.9;
         
     }
