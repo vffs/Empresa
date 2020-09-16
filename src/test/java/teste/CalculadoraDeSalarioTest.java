e241e9d4481678605b7220a46944f45ff91c87fc
vffs <vffs@a.recife.ifpe.edu.br>
2020-09-16 12:56:31 -0300

    retirado código repetido, criado um método inicializa @Before

--- src/test/java/teste/CalculadoraDeSalarioTest.java
+++ src/test/java/teste/CalculadoraDeSalarioTest.java
@@ -4,19 +4,26 @@ import empresa.CalculadoraDeSalario;
 import empresa.Cargo;
 import empresa.Funcionario;
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  *
  * @author valeria
  */
-public class CalculadoraDeSalarioTest {
 
+public class CalculadoraDeSalarioTest {
+    CalculadoraDeSalario calculadora;
+    
+    @Before
+    public void inicializa(){
+        this.calculadora = new CalculadoraDeSalario();
+    }
+    
     @Test
     public void deveCalcularSalarioParaDesenvolvedoresComSalarioAbaixoDoLimite() {
         //cenário do desconto de 10% se salario for menor que 3.000,00
-        CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
-
+       
         Funcionario desenvolvedor = new Funcionario("Mauricio", 1500.0, Cargo.DESENVOLVEDOR);
 
         double salario = calculadora.calculaSalario(desenvolvedor);
@@ -26,13 +33,20 @@ public class CalculadoraDeSalarioTest {
     @Test
     public void deveCalcularSalarioParaDesenvolvedoresComSalarioAcimaDoLimite() {
         //cenário do desconto de 20% se salario for maior que 3.000,00
-        CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
-
+       
         Funcionario desenvolvedor = new Funcionario("Mauricio", 4000.0, Cargo.DESENVOLVEDOR);
 
         double salario = calculadora.calculaSalario(desenvolvedor);
         assertEquals(4000.0 * 0.8, salario, 0.00001);
     }
 
-    
+     @Test
+    public void deveCalcularSalarioParaDBAsComSalarioAbaixoDoLimite() {
+        
+        Funcionario dba = new Funcionario("Mauricio", 500.0, Cargo.DBA);
+        
+        double salario = calculadora.calculaSalario(dba);
+        assertEquals(500.0 * 0.85, salario, 0.00001);
+    }
+
 }
